1f8717f9ae4f7b9b052a3f33cd478b89f39e0231
ByrdBassPC <byrdbass@gmail.com>
2023-07-26 10:48:12 -0500

    made some basic infered data types with var, and a public class Display with an internal String method called DisplayInfo

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace MethodsExercise
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+            Console.WriteLine("What is your favorite Animal?");
+            string animal = Console.ReadLine();
+            Display display = new Display();
+            string result = display.DisplayInfo(name, color, animal);
+            Console.WriteLine(result);
+        }
+
+
+    }
+    public class Display
+    {
+        internal string DisplayInfo(string name, string color, string animal)
+        {
+            return $"My name is {name}, and my favorite color is {color}.  My Spirit animal is the {animal}cd";
         }
     }
+
 }
